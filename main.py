0705cbb216b21062b1f77e2e4c38460f6c97b10b
baza1 <irinaerantaeva@g.mail.ru>
2024-09-17 13:52:05 +0700

    Переменные

---
+++ main.py
@@ -0,0 +1,7 @@
+name_homework = 'всего задач:'
+homework_count = '12,'
+name_hour = 'затрачено часов:'
+hour_count = '1.5,'
+name_of_course = 'Курс: Python,'
+name_comleted = 'среднее время выполнения'
+print(name_of_course, name_homework, homework_count, name_hour, hour_count,name_comleted, (float (1.5) / int (12)), 'часа.')
