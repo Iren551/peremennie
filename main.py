32337b97af19d667482be9bb1731147ee4bd8144
baza1 <irinaerantaeva@g.mail.ru>
2024-09-17 14:40:24 +0700

    Переменные

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 name_homework = 'всего задач:'
-homework_count = '12,'
+a = 12
 name_hour = 'затрачено часов:'
-hour_count = '1.5,'
+b = 1.5
 name_of_course = 'Курс: Python,'
 name_comleted = 'среднее время выполнения'
-print(name_of_course, name_homework, homework_count, name_hour, hour_count,name_comleted, (float (1.5) / int (12)), 'часа.')
+print(b/a)
+c = 0.125
+print(name_of_course, name_homework, a, name_hour, b, name_comleted, c, 'часа.')
